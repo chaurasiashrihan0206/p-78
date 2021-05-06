fa1e6901b287d6742268734adf9e996cbf6932d0
Shrihan Chaurasia <78851287+chaurasiashrihan0206@users.noreply.github.com>
2021-05-06 13:23:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var image=["https://i.pinimg.com/originals/d5/6c/d2/d56cd25436d0d3f56ad2f143750f7455.png , https://i.pinimg.com/originals/db/f5/08/dbf508e27715f8ed3730bec349092789.jpg , https://thumbs.dreamstime.com/b/cartoon-father-son-icon-over-white-background-colorful-design-vector-illustration-cartoon-father-son-icon-flat-design-169226123.jpg , https://lh3.googleusercontent.com/proxy/NzafuV4SJsamzmWod7kUtg3h1dL4rFHAptPS7D5VzhC_BbzrUSiA-EkmAcYk11fjVEvR5ki6xiNbBEfFOMzs7ubVz4kTKlk , https://static6.depositphotos.com/1000792/646/v/600/depositphotos_6464182-stock-illustration-two-boys-go-to-school.jpg"];
+var i=0;
+function nextslide()
+{document.getElementById("image1").src=image[i];
+i++;
+}
